b7290a7a81ea8619a44d28e32af0adbae3e83210
samuell1902 <146029311+samuell1902@users.noreply.github.com>
2023-09-29 14:19:21 -0300

    Arquivos que estavam na maquina da sala de aula

---
+++ aula js/assets/script.js
@@ -0,0 +1,44 @@
+let input = document.querySelector("#cpf")
+//querySelector: tag, id, classse
+//para id e classe, utilizar a mesma notação usada pelo css
+
+// () função anônima
+// => arrow functon
+// funcion nome()
+input.addEventListener("keypress", () => {
+
+let inputlength = input.value.length
+
+    if (inputlength== 3 || inputlength== 7 ) {
+        input.value += "."
+    }
+
+    else if (inputlength== 11) {
+        input.value += "-"
+    }
+})
+
+///////////////////////////////////////////////////////////////
+//cnpj
+
+let inputCnpj=document.querySelector("#cnpj")
+inputCnpj.addEventListener("keypress" , () => {
+
+let cnpjlength = inputCnpj.value.length
+if (cnpjlength== 2 || cnpjlength== 6 )
+{
+    inputCnpj.value+= "."
+} 
+if (cnpjlength==10)
+{
+    inputCnpj.value+= "/"
+}
+if (cnpjlength==15)
+{
+    inputCnpj.value+= "-"
+}
+    
+})
+
+
+    
